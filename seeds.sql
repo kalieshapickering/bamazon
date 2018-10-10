7cef114513008fa7e4c5f4dd345f2bc2a424153c
kalistan <kaliesha.pickering@gmail.com>
2018-10-10 00:10:52 -0400

    setup/structure of bamazoncustomer logic, creation of database

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES ();
